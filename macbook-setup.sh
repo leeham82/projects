7fb8271e230c7c447ac5e6e2faacf077b3ffd198
leeham82 <64020590+leeham82@users.noreply.github.com>
2021-11-28 16:41:22 +0000

    Add files via upload

---
+++ macbook-setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Setup script for setting up a new macos machine
+echo "Starting setup"
+
+# install xcode CLI
+xcode-select --install
+
+# Check for Homebrew to be present, install if it's missing
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+PACKAGES=(
+    cask
+    git
+    iterm2
+    adobe-acrobat-reader
+    visual-studio-code
+    google-chrome
+    google-drive
+    vlc
+    keka
+)
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "Macbook setup completed!"
