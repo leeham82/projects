c77b3f85ff2ed3fd10ab9a839917675780adce5f
leeham82 <liam.williamson@gmail.com>
2023-04-18 20:46:04 +0100

    Update macbook-setup.sh
    Added an else to display if Homebrew is already installed.

--- macbook-setup.sh
+++ macbook-setup.sh
@@ -10,6 +10,8 @@ xcode-select --install
 if test ! $(which brew); then
     echo "Installing homebrew..."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "Homebrew is already installed"
 fi
 
 # Update homebrew recipes
